717b37cc5fd064d72d142b13543f02b6e76d5c39
HKF1977 <30370005+HKF1977@users.noreply.github.com>
2019-03-11 14:44:06 +0000

    Update Clientsetup.sh

--- Clientsetup.sh
+++ Clientsetup.sh
@@ -3,11 +3,3 @@
 apt-get update
 
 apt-get install -y openvpn
-
-sleep 300
-
-curl -k -X GET -H 'Content-Type: application/json' -d '{"name":"100_127_255_193","format":"conf"}' https://api:vnscubed@10.10.10.10:8000/api/clientpack -o clientpack.conf
-
-mv clientpack.conf /etc/openvpn
-
-systemctl start openvpn@clientpack.service
