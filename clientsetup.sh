140aabda3dde9e07e8eae2eb333e5e80fe776040
HKF1977 <30370005+HKF1977@users.noreply.github.com>
2019-03-14 14:56:17 +0000

    Add files via upload

--- clientsetup.sh
+++ clientsetup.sh
@@ -4,7 +4,21 @@ apt-get update
 
 apt-get install -y openvpn
 
-ping -c 1 10.10.10.10 >/dev/null && curl -k -X GET -H 'Content-Type: application/json' -d '{"name":"100_127_255_193","format":"conf"}' https://api:vnscubed@10.10.10.10:8000/api/clientpack -o clientpack.conf
+wait_for_api () {
+   while :
+     do
+     apistatus=`curl -k -X GET -u api:vnscubed https://10.10.10.10:8000/api/config 2>&1`
+        echo $apistatus | grep "refused"
+          if [ $? != 0 ] ; then
+            break
+          fi
+         sleep 2
+     done
+ }
+
+wait_for_api
+
+curl -k -X GET -H 'Content-Type: application/json' -d '{"name":"100_127_255_193","format":"conf"}' https://api:vnscubed@10.10.10.10:8000/api/clientpack -o clientpack.conf
 
 mv clientpack.conf /etc/openvpn
 
